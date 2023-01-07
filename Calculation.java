827da17c3cc6cf73eb3cc8a78b1a0a814d05882c
Sanskruti Mopari <sanskrutimopari2016@gmail.com>
2023-01-07 12:54:30 +0530

    Added: Cab Invoice Generator

---
+++ Calculation.java
@@ -0,0 +1,7 @@
+public class Calculation {
+    public static void main(String[] args) {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        cabInvoiceGenerator.calculateFare(20,60, Rides.RideType.NORMAL);
+        cabInvoiceGenerator.calculateFare(20,60, Rides.RideType.PREMIUM);
+    }
+}
---
+++ InvoiceSummary.java
@@ -0,0 +1,23 @@
+public class InvoiceSummary {
+    int totalNoRide;
+    int totalFare;
+    double averageFarePerRide;
+
+    public InvoiceSummary(int totalNoRide, int totalFare) {
+        this.totalNoRide = totalNoRide;
+        this.totalFare = totalFare;
+        this.averageFarePerRide = this.totalFare / this.totalNoRide;
+    }
+
+    public int getTotalNoRide() {
+        return totalNoRide;
+    }
+
+    public int getTotalFare() {
+        return totalFare;
+    }
+
+    public double getAverageFarePerRide() {
+        return averageFarePerRide;
+    }
+}
